0a6c6241dae9bab84ea232b3a65d20a805763ad8
Jacky <jackylai1030@gmail.com>
2026-07-11 11:32:07 -0400

    created review model

---
+++ backend/models/Review.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema(
+  {
+    game: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Game",
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 10,
+    },
+    comment: {
+      type: String,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Review = mongoose.model("Review", reviewSchema);
+export default Review;
