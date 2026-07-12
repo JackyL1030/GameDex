60c5bfb1c0117a0646f63558b9d75296e6b239d8
Jacky <jackylai1030@gmail.com>
2026-07-12 13:48:30 -0400

    completed Achievement CRUD operations

--- backend/controllers/achievementController.js
+++ backend/controllers/achievementController.js
@@ -22,3 +22,50 @@ export const createAchievement = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const getAchievementById = async (req, res) => {
+  try {
+    const achievement = await Achievement.findById(req.params.id);
+    if (!achievement) {
+      return res.status(404).json({ message: "Achievement not found" });
+    }
+    res.status(200).json(achievement);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateAchievement = async (req, res) => {
+  try {
+    const achievement = await Achievement.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      },
+    );
+    if (!achievement) {
+      return res.status(404).json({
+        message: "Achievement not found",
+      });
+    }
+    res.status(200).json(achievement);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+export const deleteAchievement = async (req, res) => {
+  try {
+    const achievement = await Achievement.findByIdAndDelete(req.params.id);
+    if (!achievement) {
+      return res.status(404).json({ message: "Achievement not found" });
+    }
+    res.status(200).json({ message: "Achievement deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- backend/routes/achievementRoutes.js
+++ backend/routes/achievementRoutes.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { createAchievement, getAchievements } from "../controllers/achievementController.js";
+import {
+  createAchievement,
+  deleteAchievement,
+  getAchievementById,
+  getAchievements,
+  updateAchievement,
+} from "../controllers/achievementController.js";
 
 const router = express.Router();
 
-router.get("/", getAchievements)
-router.post("/", createAchievement)
+router.get("/", getAchievements);
+router.get("/:id", getAchievementById);
+router.post("/", createAchievement);
+router.patch("/:id", updateAchievement);
+router.delete("/:id", deleteAchievement);
 
 export default router;
