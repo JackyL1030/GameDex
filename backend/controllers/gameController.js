7d3360d24273becc44435619ca3ba5e8e0946df4
Jacky <jackylai1030@gmail.com>
2026-07-12 11:40:24 -0400

    created getGameById controller and GET route

--- backend/controllers/gameController.js
+++ backend/controllers/gameController.js
@@ -22,3 +22,15 @@ export const createGame = async (req,res) => {
         res.status(500).json({message:error.message});
     }
 }
+
+export const getGameById = async (req,res) => {
+    try {
+        const game = await Game.findById(req.params.id);
+        if(!game){
+            return res.status(404).json({message:"Game not found"})
+        }
+        res.status(200).json(game)
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+}
--- backend/routes/gameRoute.js
+++ backend/routes/gameRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createGame, getGames } from "../controllers/gameController.js";
+import { createGame, getGameById, getGames } from "../controllers/gameController.js";
 
 const router = express.Router();
 
 router.get("/", getGames);
+router.get("/:id", getGameById)
 router.post("/", createGame)
 
 export default router;
