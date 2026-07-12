5d595ddd59a048b17c0b48d9e3620cbf7fb7b7db
Jacky <jackylai1030@gmail.com>
2026-07-12 11:57:49 -0400

    created updateGame controller and PATCH route

--- backend/controllers/gameController.js
+++ backend/controllers/gameController.js
@@ -10,27 +10,42 @@ export const getGames = async (req, res) => {
   }
 };
 
-export const createGame = async (req,res) => {
-    try {
-        const platform = await Platform.findById(req.body.platform);
-        if(!platform){
-            return res.status(404).json({message:"Platform not found"})
-        }
-        const game = await Game.create(req.body);
-        res.status(201).json(game);
-    } catch (error) {
-        res.status(500).json({message:error.message});
+export const createGame = async (req, res) => {
+  try {
+    const platform = await Platform.findById(req.body.platform);
+    if (!platform) {
+      return res.status(404).json({ message: "Platform not found" });
     }
-}
+    const game = await Game.create(req.body);
+    res.status(201).json(game);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-export const getGameById = async (req,res) => {
-    try {
-        const game = await Game.findById(req.params.id);
-        if(!game){
-            return res.status(404).json({message:"Game not found"})
-        }
-        res.status(200).json(game)
-    } catch (error) {
-        res.status(500).json({message:error.message})
+export const getGameById = async (req, res) => {
+  try {
+    const game = await Game.findById(req.params.id);
+    if (!game) {
+      return res.status(404).json({ message: "Game not found" });
     }
-}
+    res.status(200).json(game);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateGame = async (req, res) => {
+  try {
+    const game = await Game.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!game) {
+      return res.status(404).json({ message: "Game not found" });
+    }
+    res.status(200).json(game);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- backend/routes/gameRoute.js
+++ backend/routes/gameRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createGame, getGameById, getGames } from "../controllers/gameController.js";
+import { createGame, getGameById, getGames, updateGame } from "../controllers/gameController.js";
 
 const router = express.Router();
 
 router.get("/", getGames);
 router.get("/:id", getGameById)
 router.post("/", createGame)
+router.patch("/:id", updateGame)
 
 export default router;
